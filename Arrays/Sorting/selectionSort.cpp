2d6110806de60e0be25e7ae6458709954464dce8
akankshr3000 <akankshr@gmail.com>
2026-08-22 18:14:18 +0530

    Learnt Selection Sorting Method

---
+++ Arrays/Sorting/selectionSort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int minIdx=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx=j;
+            }
+        }
+        swap(arr[i],arr[minIdx]);
+    }
+}
+
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[]={5,4,1,3,69}; 
+    int n=sizeof(arr)/sizeof(int);
+    selectionSort(arr,5);
+    print(arr,n);     
+    return 0;
+}
